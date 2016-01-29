4cfae19c25c14ae53c7e9ed3e7b449d5f98240fa
Akira Kashihara <kashihara@seginus.jp>
2016-01-29 10:58:10 +0900

    Fixed pass variable

--- reverse.c
+++ reverse.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 //*Reverse character order and output them
-void reverse(char *letter){                           //define argument
-  for(int i = 0; i < sizeof(letter); i++){            //repeat output function for sizeof array
-    char word = letter[sizeof(letter) - (i+1)];       //assignment word of letter to word variable from end of array.
+void reverse(char *letter, int len){                           //define argument
+ for(int i = 0; i < len; i++){            //repeat output function for sizeof array
+    char word = letter[len - (i+1)];       //assignment word of letter to word variable from end of array.
     if(word != 0){                                    //judge word is null or not.
       printf("%c\n", word);                           //output character
     }
@@ -12,7 +12,7 @@ void reverse(char *letter){                           //define argument
 
 int main(void){
   char figure[5] = {'a','b','c','d','e'};             //You can input any array
-  reverse(figure);                                    //assignment array to reverse function
+  reverse(figure, sizeof(figure));                                    //assignment array to reverse function
 
   return 0;                                           //success all tasks
 
