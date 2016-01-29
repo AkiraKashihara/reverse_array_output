6693c2cd12656d94817a1b7d3b7eae39efe2ff2a
Akira Kashihara <kashihara@seginus.jp>
2016-01-29 11:08:51 +0900

    create conf file

--- conf.c
+++ conf.c
@@ -2,16 +2,18 @@
 
 //*Reverse character order and output them
 void reverse(char *letter, int len){                           //define argument
- for(int i = 0; i < len; i++){            //repeat output function for sizeof array
+  int count = 0;
+  for(int i = 0; i < (len/2 + len%2) ; i++){            //repeat output function for sizeof array
     char word = letter[len - (i+1)];       //assignment word of letter to word variable from end of array.
-    if(word != 0){                                    //judge word is null or not.
-      printf("%c\n", word);                           //output character
+    if(word == letter[i]) {
+      count++;
     }
   }
+  printf("%d\n", count);
 }
 
 int main(void){
-  char figure[5] = {'a','b','c','d','e'};             //You can input any array
+  char figure[5] = {'a','b','c','d','a'};             //You can input any array
   reverse(figure, sizeof(figure));                                    //assignment array to reverse function
 
   return 0;                                           //success all tasks
