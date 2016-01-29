d373894f1137028294a671aeeeb7bd4356b85889
Akira Kashihara <kashihara@seginus.jp>
2016-01-29 11:02:14 +0900

    delete if

--- conf.c
+++ conf.c
@@ -2,9 +2,11 @@
 
 //*Reverse character order and output them
 void reverse(char *letter, int len){                           //define argument
-  for(int i = 0; i < len; i++){            //repeat output function for sizeof array
+ for(int i = 0; i < len; i++){            //repeat output function for sizeof array
     char word = letter[len - (i+1)];       //assignment word of letter to word variable from end of array.
-    printf("%c\n", word);                           //output character
+    if(word != 0){                                    //judge word is null or not.
+      printf("%c\n", word);                           //output character
+    }
   }
 }
 
